9e2e98ce2da8cc485f553189b032446fc32115be
Vikas Ukani <vikasukani5@gmail.com>
2021-08-20 22:33:35 +0530

    Route added default.

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController as FrontProductController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::redirect('/', 'all-products');
 
 Route::view('login', 'login')->name('login');
 Route::view('register', 'register')->name('register');
@@ -25,6 +25,5 @@ Route::group(['middleware' => ['auth',]], function () {
             Route::resource('category', CategoryController::class);
         }
     );
-
     Route::get('logout', [AuthController::class, 'logout']);
 });
